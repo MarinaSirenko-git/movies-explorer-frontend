9f86ac5e967a5c5bc8069a210f0260cdf6ca16b9
Marina Sirenko <sirenko.workspace@gmail.com>
2021-05-23 08:13:06 +0300

    добавить компонент Preloader

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,17 +1,19 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './Movies.css';
 import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Footer from '../Footer/Footer';
 
-function Movies() {
+function Movies({ isLoading }) {
   return (
     <div className="movies">
       <Header />
       <main className="movies__content">
         <SearchForm />
-        <MoviesCardList />
+        {isLoading ? <Preloader /> : <MoviesCardList />}
       </main>
       <Footer />
     </div>
